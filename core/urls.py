8dc30640b3678f3bf96901fb7401942033dc4e38
J. P. Alencar <31322402+jotapeah@users.noreply.github.com>
2021-02-12 16:47:22 +0000

    inserindo templates

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from . import views
+
 urlpatterns = [
+    path('', views.CoreView.as_view(), name='core_view'),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
